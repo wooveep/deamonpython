339d12956f672e5141dccba3ab8671b0a389af15
liyuntian <lyt.ccie@gmail.com>
2018-03-19 13:16:35 +0800

    aaa

---
+++ InstallPacharm/demon1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time      : 2018/3/19 12:31
+# @Author    : Liyuntian
+# @File      : demon1.py
+
+
+name = input("please input your name: ")
+print("hello {0}".format(name))
+
+# python2 input 输入的是整型   raw_input    输入的是字符串
+# python3 只有input   输入的是字符串
---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#@Time      : 2018/3/19 12:29
+#@Author    : Liyuntian
+#@File      : test.py
+
+
+
